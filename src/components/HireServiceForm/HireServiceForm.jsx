2b0808fb1197827581132e7f22c6146918ac253d
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-12-13 09:29:32 +0100

    Martes mañana

--- src/components/HireServiceForm/HireServiceForm.jsx
+++ src/components/HireServiceForm/HireServiceForm.jsx
@@ -1,79 +1,47 @@
 import { useState } from "react";
 import { Form, Button, Row, Col, FormGroup } from "react-bootstrap"
-import RangeSlider from 'react-bootstrap-range-slider'
-import servicesService from "../../services/services.service"
+// import RangeSlider from 'react-bootstrap-range-slider'
+// import servicesService from "../../services/services.service"
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import uploadUsers from "../../services/users.service"
 
 
-function HireServiceForm({ fireFinalActions }) {
+function HireServiceForm({ owner }) {
 
-    const [serviceData, setDataService] = useState({
-
-        serviceDuration: "",
-        serviceDate: "",
-    })
-
-    const [errors, setErrors] = useState(null)
-
-    const [minutes, setMinutes] = useState(15)
+    const [hours, setHours] = useState(0)
 
     const handleInputChange = e => {
-        const { name, value } = e.target
-        setDataService({ ...serviceData, [name]: value })
+        setHours(e.target.value)
     }
 
-    const { serviceDuration, serviceDate, } = serviceData
 
     const handleFormSubmit = e => {
 
         e.preventDefault()
-        servicesService
-            .saveService(serviceData)
-            .then(() => {
-                fireFinalActions()
-            })
-            .catch(err => setErrors(err.response.data.errorMessages))
-    }
 
+        const bankAccountTime = { hours }
+
+
+        uploadUsers
+            .updateUser(owner, bankAccountTime)
+            .then((res) => {
+                console.log(res)
+            })
+        // .catch(err => setErrors(err.response.data.errorMessages))
+    }
 
     return (
         <div>
             <Form onSubmit={handleFormSubmit}>
-
-                <Form.Group className="mb-3" controlId="serviceDate">
-                    <Form.Label>Fecha</Form.Label>
-                    <Form.Control type="date" value={serviceDate} onChange={handleInputChange} name="serviceDate" />
-
-                    <br />
-
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="serviceDuration" as={Row}>
-                    <Form.Label>Duración del servicio (minutos)</Form.Label>
-                    <Col xs="9">
-                        <RangeSlider
-                            value={minutes}
-                            onChange={e => setMinutes(e.target.value)}
-                            tooltipLabel={currentValue => `${currentValue} minutos`}
-                            variant='success'
-                            min={30}
-                            max={180}
-                            step={30}
-                        />
-                    </Col>
-                    <Col xs="3">
-                        <Form.Control value={minutes} /> <p>Minutos</p>
-                    </Col>
-                </Form.Group>
-
-                <div className="d-grid">
-                    <Button variant="dark" type="submit">Contratar Servicio</Button>
-                </div>
+                <Form.Label>bankAccountTime</Form.Label>
+                <Form.Control type="number" value={hours} onChange={handleInputChange} name="bankAccountTime" />
+                <Button variant="dark" type="submit">Contratar</Button>
 
             </Form>
 
+
         </div>
+
     )
 }
-
 export default HireServiceForm
