d04183de3c856861dfada412bfec04db8b4dca4f
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-12-13 16:50:54 +0100

    merge 2

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,59 +1,32 @@
 import './ProfilePage.css'
 import { AuthContext } from "../../contexts/auth.context"
 import { useContext } from 'react';
-import { Card, Badge, Button, Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import servicesService from "../../services/services.service"
-import ServiceDetailsPage from '../ServicesDetailsPage/ServicesDetailsPage';
 
 function ProfilePage() {
 
 
-    // const [services, setServices] = useState([])
     const { user } = useContext(AuthContext)
 
 
 
     return (
-
-
         <Container>
             <Row>
-                <Col sm={{ span: 4 }}>
+                <Col md={{ span: 4 }}>
                     <img src={user.avatar} className="avatar" />
                 </Col>
-                <Col sm={{ span: 8 }}>
+
+                <Col md={{ span: 8 }}>
                     <h2 className="userName">{user.username} </h2>
                     <hr />
                     <h2>Mis servicios</h2>
 
                 </Col>
 
-
-
             </Row>
         </Container >
-
-        // <Container >
-        //     <Card style={{ width: '18rem' }} className="profileCard">
-        //         <Card.Img variant="top" src={user.avatar} className="avatar" />
-        //         <Card.Body>
-        //             <Card.Title className="mb-3 text-center"><h3>{user.username}</h3></Card.Title>
-        //             <hr />
-        //             <Card.Subtitle className="mb-3">
-        //                 <Button variant="Light" >
-        //                     Mi tiempo para consumir es:
-        //                     <Badge bg="secondary" className="mt-3">{user.bankAccountTime = 0}</Badge>
-        //                 </Button>
-        //             </Card.Subtitle>
-        //             <div className="d-grid gap-2">
-        //                 <Link to="/servicios" >
-        //                     <Button variant="dark" size="lg">Ir a los servicios</Button>
-        //                 </Link>
-
-        //             </div>
-        //         </Card.Body>
-        //     </Card>
-        // </Container>
 
     )
 
