948df57ef156a21c75b4e5dd23d5cfac9df4cf27
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2025-07-07 10:09:41 +0200

    feat:seo

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -279,14 +279,16 @@ function ProfilePage() {
             </div>
             <div className="div5">
               {" "}
-              <h3 className="title_section-profile">SOLICITUDES PENDIENTES</h3>
+              <h3 className="title_section-profile" style={{ color: "white" }}>
+                SOLICITUDES PENDIENTES
+              </h3>
               <span className="title_section-profile-subtext">
                 de los servicios que ofreces
               </span>
               {pendingRequests.length === 0 ? (
                 <>
-                  <EmptyData />
-                  <hr />
+                  <EmptyData whiteText="true" />
+                  <hr style={{ color: "white" }} />
                 </>
               ) : (
                 <>
@@ -296,12 +298,13 @@ function ProfilePage() {
                         <>
                           <div className="card_profile-tusservicios" id={idx}>
                             <div className="card_profile_container_name">
-                              <span>{request?.service?.name}</span>
+                              <span style={{ color: "white" }}>
+                                {request?.service?.name}
+                              </span>
                             </div>
                             <div className="card_profile-body-solicitudes">
                               <CalendarSM
                                 hours={request?.hours}
-                                blackBackgroung
                                 disponibility={request?.service?.disponibility}
                               />
                               <div className="button-profile-container">
@@ -323,7 +326,7 @@ function ProfilePage() {
                               </div>
                             </div>
                           </div>
-                          <hr />
+                          <hr style={{ color: "white" }} />
                         </>
                       ))}
                     </div>
@@ -461,7 +464,7 @@ function ProfilePage() {
                             <div className="card_profile-body-solicitudes">
                               <CalendarSM
                                 hours={request?.hours}
-                                // blackBackgroung
+                                blackBackgroung
                                 disponibility={request?.service?.disponibility}
                               />
                               <div className="button-edit-container">
